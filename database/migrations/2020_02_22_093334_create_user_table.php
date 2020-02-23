997b2fde2eeee617a54e6bc6815f7b6b545dd388
bitxer <24569832+bitxer@users.noreply.github.com>
2020-02-23 13:22:48 +0800

    added database migrations

---
+++ database/migrations/2020_02_22_093334_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_0900_ai_ci';
+            $table->increments('uuid')->unsigned();
+            $table->text('loginid');
+            $table->text('password');
+            $table->text('email');
+            $table->text('name');
+            $table->tinyInteger('isadmin')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
