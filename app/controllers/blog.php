8f18cdcaeeae70d087703f150b4ce4346cc40ef4
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-09 14:39:49 +0800

    Seperate controller

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -1,33 +1,35 @@
 <?php
 
+require_once('../app/core/BlogController.php');
+
 class blog extends Controller
 {
     public static function index()
     {
-        parent::view('404');
+        Controller::view('404');
     }
 
     public static function u(...$argv){
         if (!isset($argv[0])) {
-            parent::view('404');
+            Controller::view('404');
         } else {
             $loginid = $argv[0];
             if(sizeof($argv)<=2) {
-                $UserBlogID = parent::getUserID($loginid);
+                $UserBlogID = BlogController::getUserID($loginid);
                 if ($UserBlogID == null) {
-                    parent::view('404');
+                    Controller::view('404');
                 } else {
                     if(isset($argv[1])) {
-                        $post_info = parent::getPost($UserBlogID, $PostID = $argv[1]);
+                        $post_info = BlogController::getPost($UserBlogID, $PostID = $argv[1]);
                         if ($post_info == null) {
-                            parent::view('404');
+                            Controller::view('404');
                         } else {
 
-                        parent::view('post', ['post_info' => $post_info]);
+                            Controller::view('post', ['post_info' => $post_info]);
                         }
                     }else {
-                        $blog_info = parent::getBlog($UserBlogID);
-                        parent::view('blog', ['blog_info' => $blog_info, 'blog_name' => $loginid]);
+                        $blog_info = BlogController::getBlog($UserBlogID);
+                        Controller::view('blog', ['blog_info' => $blog_info, 'blog_name' => $loginid]);
                     }
                 }
             }else{
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,33 +3,5 @@ class Controller{
     public static function view($view, $data = []){
         require_once  '../public/' . $view . '.php';
     }
-    public static function getUserID($login_id){
-        require_once("../app/model/User.php");
-        $userFound = get_user("*", ['loginid'=>['=', $login_id]]);
-        if($userFound==null){
-            return $rows = null;
-        }else {
-            return $userFound[0]->getID();
-        }
-    }
-    public static function getBlog($user_id){
-        require_once("../app/model/Post.php");
-        $rows = get_post("*",['usr_id'=>['=',$user_id]]);
-        if ($rows!=null) {
-            usort($rows, function ($a, $b) {
-                $epochA = $a->getCreated();
-                $epochB = $b->getCreated();
-                if ($epochA == $epochB) {
-                    return 0;
-                }
-                //Sort By desending order
-                return $epochA > $epochB ? -1 : 1;
-            });
-        }
-        return $rows;
-    }
-    public static function getPost($user_id,$post_id){
-        require_once("../app/model/Post.php");
-        return $rows = get_post("*",['usr_id'=>['=',$user_id],'id'=>['=',$post_id]]);
-    }
+
 }
