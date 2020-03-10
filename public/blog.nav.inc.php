16c4e9dc950658ebc568a1c66345641b8cee1bae
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-10 12:28:33 +0800

    fix blog page nav

--- public/blog.nav.inc.php
+++ public/blog.nav.inc.php
@@ -9,7 +9,8 @@
         <?php for($page=1;$page<=$data['blog_max_page'];$page++) : ?>
             <li class="page-item <?= $page==$data['blog_current_page'] ? "active" : "" ?>"><a class="page-link" href="<?=parse_url($_SERVER['REQUEST_URI'])['path'];?>?page=<?=$page;?>"><?=$page;?></a></li>
         <?php endfor;?>
-        <li class="page-item <?= $data['blog_current_page']==$data['blog_max_page'] ? "disable" : "" ?>">
+        ?>
+        <li class="page-item <?= $data['blog_current_page']==$data['blog_max_page'] ? "disabled" : "" ?>">
             <a class="page-link" href="<?=parse_url($_SERVER['REQUEST_URI'])['path'];?>?page=<?= $data['blog_current_page']>=1 ? $data['blog_current_page']+1 : $data['blog_max_page']?>" aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
                 <span class="sr-only">Next</span>
