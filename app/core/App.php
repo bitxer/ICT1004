a51fbf432394d90055e177ddda6679978207eb21
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-11 21:32:07 +0800

    *Push start session to App

--- app/core/App.php
+++ app/core/App.php
@@ -19,27 +19,30 @@ class App{
         $this->SetMethodName();
         $this->SetParam();
         call_user_func_array([$this->controller,$this->method], $this->params);
-
     }
+
     public function parseUrl(){
         $this->url = parse_url($this->url);
         $this->url = explode('/',$this->url['path'], FILTER_SANITIZE_URL);
         unset($this->url[0]);
     }
+
     public function SetPageName(){
         if(file_exists('../app/controllers/' . $this->url[1] . '.php')) {
             $this->controller = $this->url[1];
             unset($this->url[1]);
         }
     }
+
     public function SetMethodName(){
-            if(isset($this->url[2])){
-                if(method_exists($this->controller, $this->url[2])){
-                    $this->method = $this->url[2];
-                    unset($this->url[2]);
-                }
+        if(isset($this->url[2])){
+            if(method_exists($this->controller, $this->url[2])){
+                $this->method = $this->url[2];
+                unset($this->url[2]);
             }
         }
+    }
+
     public function SetParam(){
         $this->params = $this->url ? array_values($this->url) : [];
     }
