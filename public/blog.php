57cbd983ac8e20c3d2cb6d0b8f089567df68f283
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-12 21:42:20 +0800

    fix border

--- public/blog.php
+++ public/blog.php
@@ -1,33 +1,17 @@
-<html>
-<!--Temp head-->
-    <head>
-        <link rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-              integrity=
-              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-              crossorigin="anonymous">
-        <script defer
-                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-                integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-                crossorigin="anonymous">
-        </script>
-        <link rel="stylesheet"
-        href="/static/css/style.css">
-    </head>
-    <body class="container-fluid">
-        <section>
-            <article>
-                <div class="card border-light mb-3">
-                    <main class="card-body">
-                        <h2>Welcome to <?=$data['blog_name']?>'s blog!</h2>
-                    </main>
-                </div>
-            </article>
-        </section>
+         <section class="card border-0">
+             <article class="border-bottom pt-3 mb-3">
+                 <header class="card-body">
+                     <h2 class="text-center">Welcome to <?=$data['blog_name']?>'s blog!</h2>
+                 </header>
+             </article>
+         </section>
         <section class="card m-5">
             <?php if(isset($data['blog_info'])) : ?>
                 <header class="card-header">
-                    <?php include 'blog.nav.inc.php'?>
+                    <section class="row">
+                        <span class="col-2 pt-2 pl-5"><h2>Blog Post</h2></span>
+                    <span class="col"><?php include 'blog.nav.inc.php'?></span>
+                    </section>
                 </header>
                 <?php if(isset($data['blog_info']))  : ?>
                     <?php foreach ($data['blog_info'] as &$entry) : ?>
@@ -81,6 +65,3 @@
                     </div>
                 </article>
             <?php endif;?>
-        </section>
-    </body>
-</html>
