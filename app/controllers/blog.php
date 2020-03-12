1cc6d356e5d38ab8d3c830dea4dc289b4c3622ab
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-12 23:18:01 +0800

    add session destroy

--- app/controllers/blog.php
+++ app/controllers/blog.php
@@ -13,38 +13,38 @@ class blog extends Router
      */
     public static function index()
     {
-        Router::view(['page'=>'404']);
+        self::view(['page'=>'404']);
     }
 
     public static function u(...$argv){
         if (!isset($argv[0])) {
-            Router::view(['page'=>'404']);
+            self::view(['page'=>'404']);
         } else {
             $loginid = $argv[0];
             if(sizeof($argv)<=2) {
                 $UserBlogID = BlogController::getUserID($loginid);
                 if ($UserBlogID == null) {
-                    Router::view(['page'=>'404']);
+                    self::view(['page'=>'404']);
                 } else {
                     if(isset($argv[1])) {
                         $post_info = BlogController::getPost($UserBlogID, $PostID = $argv[1]);
                         if ($post_info == null) {
-                            Router::view(['page'=>'404']);
+                            self::view(['page'=>'404']);
                         } else {
-                            Router::view(['page'=>'post','post_info' => $post_info, 'blog_name'=>$loginid]);
+                            self::view(['page'=>'post','post_info' => $post_info, 'blog_name'=>$loginid]);
                         }
                     }else {
                         $blog_info = BlogController::getBlog($UserBlogID);
                         $blog_by_page = BlogController::getBlogbyPageX($blog_info);
                         if(!isset($blog_by_page['row'])){
-                            Router::view(['page'=>'blog','blog_name'=>$loginid,'blog_max_page'=>$blog_by_page['max_page']]);
+                            self::view(['page'=>'blog','blog_name'=>$loginid,'blog_max_page'=>$blog_by_page['max_page']]);
                         }else{
-                        Router::view(['page'=>'blog','blog_info' => $blog_by_page['row'],'blog_current_page'=>$blog_by_page['cur_page'], 'blog_max_page'=> $blog_by_page['max_page'], 'blog_name' => $loginid]);
+                        self::view(['page'=>'blog','blog_info' => $blog_by_page['row'],'blog_current_page'=>$blog_by_page['cur_page'], 'blog_max_page'=> $blog_by_page['max_page'], 'blog_name' => $loginid]);
                         }
                     }
                 }
             }else{
-                Router::view(['page'=>'404']); //e.g /u/test/2/randomstring
+                self::view(['page'=>'404']); //e.g /u/test/2/randomstring
             }
         }
     }
@@ -53,11 +53,12 @@ class blog extends Router
             if($_POST){
                 if($token_match = Router::token_compare()){
                     $postsuccess = BlogController::AddPost($_POST);//$postsuccess returns an array if an entry is invalid, bool if it is success
+                    var_dump($postsuccess);
                     if(is_bool($postsuccess)){
                         header("Location: /blog/u/" . $_SESSION['loginid']);
 
                     }else{
-                        Router::view(['page'=>'create']);
+                        self::view(['page'=>'create']);
                     }
                 }else{
                     session_destroy();
@@ -65,10 +66,11 @@ class blog extends Router
                 }
 
             }else {
-                Router::view(['page'=>'create']);
+                self::view(['page'=>'create']);
             }
         }else{
             //Go back to Login
+            session_destroy();
             header("Location: /");
         }
     }
