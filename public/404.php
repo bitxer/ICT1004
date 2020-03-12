48fdb18e4c68ff0700e32cc35633d2d141ca5ad8
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-12 21:43:07 +0800

    Templating

--- public/404.php
+++ public/404.php
@@ -1,18 +1,4 @@
-<!--Temp head-->
-<html>
-    <head>
-        <link rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-              integrity=
-              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-              crossorigin="anonymous">
-        <script defer
-                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-                integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
-                crossorigin="anonymous">
-        </script>
-    </head>
-    <body>
+
         <main>
             <article>
                 <div class="card text-center">
@@ -26,5 +12,3 @@
                 </div>
             </article>
         </main>
-    </body>
-</html>
