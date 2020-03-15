fbf9c3a6cf07263d534dfee025f624dcac3e279d
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-16 00:36:33 +0800

    change html tagging

--- public/create.php
+++ public/create.php
@@ -3,16 +3,16 @@
             <legend class="form-group">Submit new post</legend>
             <fieldset class="row">
                 <div class="col">
-                    <fieldset class="form-group">
+                    <div class="form-group">
                         <input type="text" name="title" class="form-control" placeholder="Title">
-                    </fieldset>
-                    <fieldset class="form-group">
+                    </div>
+                    <div class="form-group">
                         <textarea class="form-control post-box" name="content" rows="18" placeholder="Add a Post here..."></textarea>
-                    </fieldset>
-                    <fieldset class="form-group">
+                    </div>
+                    <div class="form-group">
                         <input type="hidden" name="token" value="<?=$_SESSION['token']?>">
                         <button class="btn btn-primary" id="submit">Submit new post</button>
-                    </fieldset>
+                    </div>
                 </div>
             </fieldset>
         </form>
