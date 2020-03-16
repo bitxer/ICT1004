d264e08889c16237b16e81cbbe4511403d4daaba
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-16 13:44:49 +0800

    added session constants

--- app/constants.php
+++ app/constants.php
@@ -5,6 +5,11 @@ define("APP_ROOT", trim(dirname($_SERVER["REQUEST_URI"])));
 define("APP_DOMAIN", trim($_SERVER["SERVER_NAME"]));
 define("APP_TZ", "Asia/Singapore");
 
+// Session ID key
+define("SESSION_UID", "S_UID");
+define("SESSION_UNAME", "S_UNAME");
+define("SESSION_RIGHTS", "S_RIGHTS");
+
 # Sensitive parameters.
 require_once("app/private/database.php");
 ?>
