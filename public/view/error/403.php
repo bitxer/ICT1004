193bc2ff249dcc3534159f87ab07a211df7beeea
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-18 22:10:10 +0800

    added error codes

---
+++ public/view/error/403.php
@@ -0,0 +1,14 @@
+
+        <main>
+            <article>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Error 403
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Sorry, this page isn't available</h5>
+                        <p class="card-text">You may not have permission for that resource.</p>
+                    </div>
+                </div>
+            </article>
+        </main>
---
+++ public/view/error/440.php
@@ -0,0 +1,14 @@
+
+        <main>
+            <article>
+                <div class="card text-center">
+                    <div class="card-header">
+                        Error 440
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Login Timeout</h5>
+                        <p class="card-text">Please Sign in again</p>
+                    </div>
+                </div>
+            </article>
+        </main>
