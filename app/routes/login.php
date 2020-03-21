bd23ff2298b86d1a3b253797b1ebc5cfc5b2d7d6
haha <reuben.ong.exe@gmail.com>
2020-03-21 22:23:04 +0800

    Added login prompts, comments, checks if user is suspended, andupdated login routes

--- app/routes/login.php
+++ app/routes/login.php
@@ -1,6 +1,13 @@
 <?php
 require_once '../app/controllers/LoginController.php';
 require_once '../app/utils/helpers.php';
+/*
+ login_process will call the LoginController class's getUserAccount
+ function. If the array returned is NULL,  redirect to login page with 
+ invalid credentials error. If account is suspended, redirect to login page 
+ with account locked error. If it's an admin account, redirect to admin page.
+ Otherwise, redirect to the user's blog page.
+*/
 class login extends Router
 {
     public function index()
@@ -9,19 +16,24 @@ class login extends Router
     }
     public function login_process()
     {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            $this->abort(403);
+        }
         $login_control = new LoginController();
-        get($_POST['token']);
-        if ($this->token_compare()) {
+       
             $account = $login_control->getUserAccount();
             if ($account == NULL) {
-                header("Location: /login");
-            } else {
-                $_SESSION['loginid'] = $account;
+                header("Location: /login?error=invalidcredentials");
+            } else if($account['isadmin'] == 1) {
+                header("Location: /admin/dashboard");
+            } else if($account['suspended'] == 1){
+                header("Location: /login?error=accountlocked");
+            }
+            else {
+                $_SESSION['loginid'] = $account['loginid'];
                 header("Location: /blog/u/" . $_SESSION['loginid']);
             }
-        } else {
-            session_destroy();
-            header("Location: /login");
-        }
-    }
-}
+        } 
+    } 
+
+
--- public/static/js/validate.js
+++ public/static/js/validate.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 function validatePassword(){
     var pass1 = $('#password');
     var pass2 = $('#confirm_password');
-    //var submit_btn = document.getElementById("register-user");
     $('#password, #confirm_password').on('keyup', function () {
         if (pass2.val() == pass1.val()) {
           $('#message1').html('✔').css('color', 'green');
