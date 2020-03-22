1966ab1f98d1b7dffa476d038af79ae3d35b9c80
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-22 21:48:56 +0800

    added output sanitisation

--- app/view/admin/setup.php
+++ app/view/admin/setup.php
@@ -19,20 +19,20 @@
             <div class="form-group">
                 <label for="loginid">Username</label>
                 <div>
-                    <input type="text" title="This will be your profile name." class="form-control" required name="loginid" id="loginid" value="<?php if (isset($_POST['loginid'])) echo $_POST['loginid']; ?>">
+                    <input type="text" title="This will be your profile name." class="form-control" required name="loginid" id="loginid">
                 </div>
             </div>
             <div class="form-group">
                 <label>Name</label>
                 <div>
-                    <input type="text" title="Your full name." class="form-control" required id="name" name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>">
+                    <input type="text" title="Your full name." class="form-control" required id="name" name="name">
                 </div>
 
             </div>
             <div class="form-group">
                 <label>Email</label>
                 <div>
-                    <input type="text" class="form-control" required pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" id="email" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+                    <input type="text" class="form-control" required pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" id="email" name="email">
                 </div>
             </div>
 
--- app/view/admin/users/specific.php
+++ app/view/admin/users/specific.php
@@ -11,19 +11,19 @@ $suspended = $user->getField('suspended')->getValue() == 1;
             <tbody>
                 <tr>
                     <th scope="col" class="w-25 text-right">Unique User ID:</th>
-                    <td scope="col"><?= $user->getField('id')->getValue(); ?></td>
+                    <td scope="col"><?= htmlspecialchars($user->getField('id')->getValue()); ?></td>
                 </tr>
                 <tr>
                     <th scope="col" class="w-25 text-right">Username:</th>
-                    <td scope="col"><?= $user->getField('loginid')->getValue(); ?></td>
+                    <td scope="col"><?= htmlspecialchars($user->getField('loginid')->getValue()); ?></td>
                 </tr>
                 <tr>
                     <th scope="col" class="w-25 text-right">Email:</th>
-                    <td scope="col"><?= $user->getField('email')->getValue(); ?></td>
+                    <td scope="col"><?= htmlspecialchars($user->getField('email')->getValue()); ?></td>
                 </tr>
                 <tr>
                     <th scope="col" class="w-25 text-right">Name: </th>
-                    <td scope="col"><?= $user->getField('name')->getValue(); ?></td>
+                    <td scope="col"><?= htmlspecialchars($user->getField('name')->getValue()); ?></td>
                 </tr>
                 <tr>
                     <th scope="col" class="w-25 text-right">Admin user: </th>
@@ -33,14 +33,14 @@ $suspended = $user->getField('suspended')->getValue() == 1;
                     <td scope="col"><?= $suspended ? 'Yes' : 'No'; ?></td>
                 </tr>
                 <tr>
-                    <td scope="col" colspan="2" class="text-center">Click <a href="/blog/u/<?= $user->getField('loginid')->getValue(); ?>">here</a> to view blog</td>
+                    <td scope="col" colspan="2" class="text-center">Click <a href="/blog/u/<?= htmlspecialchars($user->getField('loginid')->getValue()); ?>">here</a> to view blog</td>
                 </tr>
             </tbody>
         </table>
         <!-- <form class="text-center" id="userAction"action="/admin/action" method='POST'> -->
         <form class="text-center" id="userAction">
             <input type="hidden" name="<?= $_SESSION[FORM_CSRF_FIELD]; ?>" value="<?= $_SESSION[SESSION_CSRF_TOKEN]; ?>">
-            <input type="hidden" name="uid" value="<?= $user->getField('id')->getValue(); ?>">
+            <input type="hidden" name="uid" value="<?= htmlspecialchars($user->getField('id')->getValue()); ?>">
             <?php if ($isadmin) { ?>
                 <input type="submit" name="demote" class="btn btn-danger" value="Remove admin rights"/>
             <?php } else  { ?>
