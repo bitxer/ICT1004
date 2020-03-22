c7647bd481431e15f20e3ccefbe19dd007a65cba
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-23 02:11:29 +0800

    added http error 400 page

---
+++ app/view/error/400.php
@@ -0,0 +1,9 @@
+<section class="card m-3 text-center">
+    <header class="card-header">
+        Error 400
+    </header>
+    <main class="card-body">
+        <h5 class="card-title">Bad Request</h5>
+        <p class="card-text">The request could not be understood by the server due to malformed syntax.</p>
+    </main>
+</section>
