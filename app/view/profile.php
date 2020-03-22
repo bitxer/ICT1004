974be0e99f8b81f09c059951e2408b8bd8fc8592
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-23 03:01:58 +0800

    formatted and removed some redundant code

--- app/view/profile.php
+++ app/view/profile.php
@@ -1,22 +1,21 @@
-
 <section class='card border-dark mb-3' id="profile" style="max-width: 35rem; ">
-<h1 class="card-header">My Profile</h1>
+    <h1 class="card-header">My Profile</h1>
 
     <form class="profile card-body" method="post" action="/account/update_profile">
 
         <?php
-        if (!empty($_SESSION['msg'])) {
-            echo "<div id='myMsg'>" . $_SESSION['msg'] . "</div>";
-            unset($_SESSION['msg']);
-        }
+            if (!empty($_SESSION['msg'])) {
+                echo "<div id='myMsg'>" . $_SESSION['msg'] . "</div>";
+                unset($_SESSION['msg']);
+            }
         ?>
         <br>
 
         <label>User Id:</label>
         <article class="form-group">
             <?php
-            $user = $data['loginid'];
-            echo '<input class="form-group" type="text" name="userid" placeholder="Enter Your user id" maxlength="50" value=' . $user . ' aria-label="loginid">';
+                $user = $data['loginid'];
+                echo '<input class="form-group" type="text" name="userid" placeholder="Enter Your user id" maxlength="50" value=' . $user . ' aria-label="loginid">';
             ?>
             <button class="btn btn-primary" type="submit" name="update" value="buserid">Update User id</button>
         </article>
@@ -24,8 +23,8 @@
         <label>Email:</label>
         <article class="form-group">
             <?php
-            $email = $data['email'];
-            echo '<input class="form-group" type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="Enter Your new Email" maxlength="100" value=' . $email . ' aria-label="email">';
+                $email = $data['email'];
+                echo '<input class="form-group" type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="Enter Your new Email" maxlength="100" value=' . $email . ' aria-label="email">';
             ?>
             <button class="btn btn-primary" type="submit" name="update" value="bemail">Update Email</button>
         </article>
@@ -33,8 +32,8 @@
         <label>Name:</label>
         <article class="form-group">
             <?php
-            $name = $data['name'];
-            echo '<input class="form-group" type="text" name="name" placeholder="Enter Your new Name" maxlength="50"     value=' . $name . ' aria-label="name">';
+                $name = $data['name'];
+                echo '<input class="form-group" type="text" name="name" placeholder="Enter Your new Name" maxlength="50"     value=' . $name . ' aria-label="name">';
             ?>
             <button class="btn btn-primary" type="submit" name="update" value="bname">Update Name</button>
         </article>
@@ -56,6 +55,6 @@
         </article>
 
         <label id="profilenote">Note: if you change your password, you will be signed out, sign back in with the new password</label>
-        <input type="hidden" name="<?=FORM_CSRF_FIELD?>" value="<?=$_SESSION[SESSION_CSRF_TOKEN]?>">
+        <input type="hidden" name="<?= FORM_CSRF_FIELD ?>" value="<?= $_SESSION[SESSION_CSRF_TOKEN] ?>">
     </form>
 </section>
