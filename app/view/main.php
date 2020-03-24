06db8e393bdd96da2adea5c2938da12b81559b24
Ronnie Tang <30373651+RonnieTJW99@users.noreply.github.com>
2020-03-24 20:35:27 +0800

    add timeout error

--- app/view/main.php
+++ app/view/main.php
@@ -1,4 +1,15 @@
 <section class=" main-background intro-blog">
+
+<?php if ($_GET['timeout']):?>
+    <article class="card border-0 intro-head p-5">
+        <div class="alert alert-danger alert-dismissible fade show mt-2 mb-0 alert-box comment-alert" role="alert">
+            <h5 class="text-center">Your Session has timeout!! Please sign in again</h5>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </article>
+<?php endif; ?>
     <h2 class="intro-head m-0 p-5 text-center">Welcome to Budget Blogspot</h2>
     <div class="row m-0 intro-blog">
         <article class="col">
