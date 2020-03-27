da4b70e51dae4d589adfab067483ed8c72b93b4b
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-27 22:55:32 +0800

    removed ajax for delete of contact request

--- public/static/js/admin/action.js
+++ public/static/js/admin/action.js
@@ -13,19 +13,3 @@ $("#userAction").submit(function(e){
         }
     );
 });
-
-$("#contactAction").submit(function(e){
-    e.preventDefault();
-    var post_url = "/admin/delete";
-    var form_data = $(this).serialize();
-    var btn = $(this).find("input[type=submit]:focus").attr('name');
-    form_data += "&button=" + btn;
-    $.post(
-        post_url,
-        form_data,
-        function(data, status){
-		    alert("Successfully delete contact request");
-            location.reload();
-        }
-    );
-});
