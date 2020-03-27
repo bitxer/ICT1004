3db6f02db7ef75dd6d34a870d26677aac52414cc
bitxer <24569832+bitxer@users.noreply.github.com>
2020-03-27 22:17:55 +0800

    prevent user from updating

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -44,6 +44,13 @@ class AccountController
         $val = '';
         $keyval = (array) null;
         $msg = '';
+        $data = get_user('*', ['loginid' => ["=", $uid]]);
+
+        if ($data !== NULL) {
+            $_SESSION['msg'] = "A user with the given User ID already exists";
+            return;
+        }
+        
         switch ($_POST["update"]) {
                 //when update user id button is pressed
             case 'bprofile':
@@ -51,8 +58,6 @@ class AccountController
                     $errorMsg .= "User id is required <br>";
                     $success = false;
                 } else {
-                    $key = 'loginid';
-                    $val = $uid;
                     $keyval['loginid'] = $uid;
                     $msg = 'Profile';
                     $success = true;
@@ -67,9 +72,6 @@ class AccountController
                         $errorMsg .= "Invalid email format.<br>";
                         $success = false;
                     } else {
-                        echo $email;
-                        $key = 'email';
-                        $val = $email;
                         $keyval['email'] = $email;
                         $msg = 'Profile';
                         $success = true;
@@ -80,8 +82,6 @@ class AccountController
                     $errorMsg .= "Name is required <br>";
                     $success = false;
                 } else {
-                    $key = 'name';
-                    $val = $name;
                     $keyval['name'] = $name;
                     $msg = 'Profile';
                     $success = true;
@@ -95,9 +95,7 @@ class AccountController
                 } else {
                     if (password_verify($cpwd,$currentpwd)) {
                         if ($npwd == $ncpwd) {
-                            $key = 'password';
-                            $hash = password_hash($npwd, PASSWORD_DEFAULT);
-                            $val = $hash;
+                            $keyval['key'] = password_hash($npwd, PASSWORD_DEFAULT);
                             $msg = 'Password';
                             $p = true;
                             $success = true;
@@ -116,7 +114,6 @@ class AccountController
 
 
         if ($success) {
-            $values = [$key => $val];
             foreach ($keyval as $key => $val) {
                 if ($val != null || $val != " ") {
                     $user->setValue($key, $val);
@@ -125,7 +122,6 @@ class AccountController
             
             $_SESSION[SESSION_LOGIN] = $uid == "" ? $_SESSION[SESSION_LOGIN] : $uid;
             $msg .= " have been successfully updated <br>";
-            //$_SESSION['msg'] = $msg;
             if($p == true){
                 $msg.="Your updated password will take effect after next login";
             }
